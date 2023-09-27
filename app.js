fa983c6cfff98e06e8c2b9f08807e76a7adde5d1
ismailattari <136378019+ismailattari@users.noreply.github.com>
2023-09-27 15:36:35 +0500

    js-alerts-assignments1

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error!Please enter the valid password");
+alert("welcome to js lend...\n Happy Coding!");
+alert("welcome to js lend...");
+alert("Happy Coding!\n Prevent this page from creating additional dialogs.");
+alert("Hello... I can run JS through my web browser`s console");
